b6426d4b44642aa0a454abf5f78c2501b22b5e3a
Sjce <hebbarharish04@gmail.com>
2025-01-30 17:03:46 +0530

    Update prog2.tcl

--- prog2.tcl
+++ prog2.tcl
@@ -1,5 +1,5 @@
-2)Implement transmission of ping messages/trace route over a network topology
-consisting of 6 nodes and find the number of packets dropped due to congestion.
+## 2)Implement transmission of ping messages/trace route over a network topology
+##   consisting of 6 nodes and find the number of packets dropped due to congestion.
 -->
 set ns [new Simulator]
 
